22513b850942292e2c2864858c582c2e2a6ab2e1
shinyflvre <me@shinyflvres.com>
2026-04-27 17:30:17 +0200

    Remove DEV State

--- main/AppInfo.cs
+++ main/AppInfo.cs
@@ -2,8 +2,7 @@
 
 public static class AppInfo
 {
-    // public const string Version = "2026.18.10";
-    public const string Version = "DEVELOPEMENT_SHINYFLVRES_";
+    public const string Version = "2026.18.10";
     public const string ContactEmail = "vrcn@shinyflvres.com";
     public const string Website = "vrcn.shinyflvres.com";
     public const string UserAgent = $"VRCNext/{Version} ({ContactEmail})";
